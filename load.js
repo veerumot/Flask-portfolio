d6e0e7c320fcd0528a3f38b11de69cd7bf907a00
veerumot <veerender.veeru2009@yahoo.com>
2024-05-07 22:59:23 -0400

    changes for new site

---
+++ load.js
@@ -0,0 +1,10 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+export const options = {
+  vus: 10,
+  duration: '30s',
+};
+export default function () {
+  http.get('https://veerender-mothukuri.com');
+  sleep(1);
+}
---
+++ test.py
@@ -0,0 +1,14 @@
+!#/bin/sh/python
+
+domain = veerender-mothukuri.com
+
+if site == domain:
+
+print(home.tittle)
+
+
+
+Output:
+
+                    Hey, Veerender Mothukuri Here!
+                   "Welcome to my portfolio – where ideas come to life through design and innovation."
